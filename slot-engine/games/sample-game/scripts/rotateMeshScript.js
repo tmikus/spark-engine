c97be07a5c1ead05b8a924d2080c031616adc149
Tomasz Mikus <Tomasz.Mikus@nektan.com>
2014-11-28 15:48:26 +0000

    Refactored scripts to use Actor's transform property.

--- slot-engine/games/sample-game/scripts/rotateMeshScript.js
+++ slot-engine/games/sample-game/scripts/rotateMeshScript.js
@@ -19,25 +19,19 @@ RotateIceCreamActorScript.prototype = Class.extend(BaseActorScript,
      */
     m_speed: 30,
     /**
-     * Transform component.
-     * @type {TransformComponent}
-     */
-    m_transformComponent: null,
-    /**
      * Called every time game requests the logic to be updated.
      *
      * @param {GameTime} gameTime Time of the game.
      */
     vOnUpdate: function vOnUpdate(gameTime)
     {
-        this.m_transformComponent.m_rotation.y += gameTime.m_deltaTime * this.m_speed * DEGREES_TO_RADIANS;
+        this.m_actor.m_transform.m_rotation.y += gameTime.m_deltaTime * this.m_speed * DEGREES_TO_RADIANS;
     },
     /**
      * Called after the script was initialised.
      */
     vPostInitialise: function vPostInitialise()
     {
-        this.m_transformComponent = this.m_actor.getComponentByName("TransformComponent");
     }
 });
 
